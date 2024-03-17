1e67c782d800d4dd17e6c27f10d12d587b788e16
dhirajamrutkar <dhirajpamrutkar@gmail.com>
2024-03-17 09:09:10 +0530

    model classes

--- payment/src/main/java/com/wallet/payment/controller/UserController.java
+++ payment/src/main/java/com/wallet/payment/controller/UserController.java
@@ -1,4 +1,17 @@
 package com.wallet.payment.controller;
 
+import lombok.*;
+
+import javax.annotation.processing.Generated;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
 public class UserController {
+    private String phoneNumber;
+    private String firstName;
+    private String lastName;
+    private String address;
+    private Long bankAccountNumber;
+
 }
